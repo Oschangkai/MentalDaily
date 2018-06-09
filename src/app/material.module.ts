f5f9a2677017b5ef0185b54f12b02c70c7fb2d5f
1041759 <gasperiop3210@gmail.com>
2018-06-10 03:28:11 +0800

    first verson for login without any design

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatToolbarModule, MatCardModule, MatInputModule } from '@angular/material';
 
 @NgModule({
-  imports: [BrowserAnimationsModule],
-  exports: [BrowserAnimationsModule],
+  imports: [BrowserAnimationsModule, MatButtonModule, MatToolbarModule, MatCardModule, MatInputModule],
+  exports: [BrowserAnimationsModule, MatButtonModule, MatToolbarModule, MatCardModule, MatInputModule],
   declarations: []
 })
 export class MaterialModule { }
