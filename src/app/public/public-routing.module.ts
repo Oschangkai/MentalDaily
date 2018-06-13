95321595cdcda93420f4f9288719709389f38881
1041759 <gasperiop3210@gmail.com>
2018-06-13 12:07:12 +0800

    worng valid in register

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PublicComponent } from './public.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component'
 
 const routes: Routes = [
   { path: '', component: PublicComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
